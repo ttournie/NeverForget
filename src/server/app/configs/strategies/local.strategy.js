d9af58d7660b5080baecd0239650a9087034d915
Thierry Tournie <thierrytournie@comhem.com>
2019-06-02 09:35:00 +0200

    fix misspeling in login passport

--- src/server/app/configs/strategies/local.strategy.js
+++ src/server/app/configs/strategies/local.strategy.js
@@ -15,11 +15,13 @@ module.exports = function localStrategy(db) {
                 const userByUsername = await db.collection('users').findOne(user);
 
                 if (userByUsername) {
-                    bcrypt.compare(password, userByUsername.passport, function(err) {
+                    bcrypt.compare(password, userByUsername.password, function(err, same) {
                         if (err) {
-                            done(null, userByUsername);
-                        } else {
                             done(null, false);
+                        } else if (!same) {
+                            done(null, false);
+                        } else {
+                            done(null, userByUsername);
                         }
                       });
                 } else {
