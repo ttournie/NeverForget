3d87196c226b3b531d094ad85df0ed3bf2ff9a94
Thierry Tournie <thierrytournie@comhem.com>
2019-06-19 15:20:15 +0200

    adding more test for note controller

--- src/server/test/noteController.js
+++ src/server/test/noteController.js
@@ -6,6 +6,7 @@ const chai = require('chai');
 const controller = require('../app/controllers/noteController');
 const app = require('../server');
 const userModel = require('../app/models/user');
+const noteModel = require('../app/models/note');
 const testData = require('./testSetup');
 
 chai.use(chaiHttp);
@@ -23,16 +24,41 @@ describe('noteController', () => {
           done();
         });
     });
-    it.skip('Should return user notes', async () => {
+    it('Should return user notes array', async () => {
       const user = await userModel.findOne({ username: testData.testUser.username });
       const req = {
         user,
       };
+      const send = sinon.spy();
       const res = {
-        send: sinon.stub,
+        send,
       };
-      controller.getAllNotes(req, res);
-      res.send.should.have.been.called;
+      await controller.getAllNotes(req, res, sinon.stub);
+      send.should.have.been.calledWith(sinon.match.array);
+    });
+  });
+  describe('Get a note', () => {
+    it('Sould return 401', (done) => {
+      chai.request(app)
+        .get('/notes/testing')
+        .end((err, res) => {
+          res.should.have.status(401);
+          done();
+        });
+    });
+    it('Should a note', async () => {
+      const note = await noteModel.findOne({ title: testData.note1.title });
+      const req = {
+        params: {
+          id: note._id,
+        },
+      };
+      const send = sinon.spy();
+      const res = {
+        send,
+      };
+      await controller.getNote(req, res, sinon.stub);
+      send.should.have.been.calledWith(sinon.match.has('title', testData.note1.title));
     });
   });
 });
